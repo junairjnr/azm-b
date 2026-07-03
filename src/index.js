1ef3aa28f0c626df2117c0f18d894a338e3cccf5
junairjnr <junairjunu869@gmail.com>
2026-07-03 11:45:53 +0530

    vercel url added

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
+  "https://azm-f.vercel.app/",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
